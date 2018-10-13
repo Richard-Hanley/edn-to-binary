f0baeaa1e8746e6a60dd8925e0788a7b716511e6
Richard Hanley <richard@rhanley.com>
2018-10-13 16:25:22 -0500

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to edn-to-binary
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/edn_to_binary/core_test.clj
@@ -0,0 +1,7 @@
+(ns edn-to-binary.core-test
+  (:require [clojure.test :refer :all]
+            [edn-to-binary.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
