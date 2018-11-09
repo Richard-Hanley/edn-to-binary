3faccdfb402da1dcc04b9b9e44c2a62e9e9cec92
Richard Hanley <richard@rhanley.com>
2018-11-09 15:28:18 -0600

    Creating a serialize function that can manage alignment

--- src/edn_to_binary/core.clj
+++ src/edn_to_binary/core.clj
@@ -1,6 +1,70 @@
 (ns edn-to-binary.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+; (defprotocol Codec
+;   (alignment* [this])
+;   (encode* [this data])
+;   (decode* [this binary])
+;   (encoding* [this] "Returns the encoding map of the passed Codec")
+;   (encoding-format* [this])
+  ; (recode [this new-encoding] "Returns a new Codec with a the new encoding"))
+
+; (defn encode [codec data])
+
+; (defn decode [codec binary])
+
+; (defn indexed-binary-xform []
+;   (map-indexed vector))
+
+; ; (defn serialize [binary] (byte-array (mapcat unchecked-byte binary)))
+; (defn serialize [binary] (byte-array (map unchecked-byte binary)))
+
+(defprotocol BinaryField
+  (binary-seq [this meta]))
+
+(extend-protocol BinaryField
+  Byte
+  (binary-seq [this _] (cons this '()))
+  Short
+  (binary-seq [this _] (cons (unchecked-byte this) '()))
+  Integer
+  (binary-seq [this _] (cons (unchecked-byte this) '()))
+  Long
+  (binary-seq [this _] (cons (unchecked-byte this) '())))
+
+
+(defn alignment-padding [align-to position]
+  (let [bytes-over (mod position align-to)]
+    (if (pos? bytes-over)
+      (- align-to bytes-over)
+      0)))
+
+
+(defrecord Alignment [align-to]
+  BinaryField
+  (binary-seq [this meta]
+    (let [{:keys [position] :or {position 0}} meta
+          padding (alignment-padding align-to position)]
+      (repeat padding (byte 0)))))
+
+
+(defn align [align-to]
+  (map->Alignment {:align-to align-to}))
+
+(defn 
+  serialize
+  "Returns a sequence of Bytes when passed a collection binary data.  Returns a transducer
+  when no collection is provided"
+  ([]
+   (fn [xf]
+     (let [length (volatile! 0)]
+       (fn 
+         ([] (xf))
+         ([result] (xf result))
+         ([result input] 
+          (let [current-length @length
+                input-bytes (binary-seq input {:position current-length})]
+            (vreset! length (+ (count input-bytes) current-length))
+            (reduce xf result input-bytes)))))))
+   ([binary-stream]
+   (sequence (serialize) binary-stream)))
--- test/edn_to_binary/core_test.clj
+++ test/edn_to_binary/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [edn-to-binary.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest simple-serialization
+  (let [expected-bytes (byte-array (map unchecked-byte (range 10)))]
+    (testing "single binary collection"
+      (is (= expected-bytes (serialize (range 10)))))
+    (testing "nested binary collections"
+      (is (= expected-bytes (serialize [[0] [1 2 3] [4 5] [6 7 8 9]]))))
+          ))
+
+
+; (deftest a-test
+;   (testing "FIXME, I fail."
+;     (is (= 0 1))))
