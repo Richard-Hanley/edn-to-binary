42d81c353e42a9c4090abf9653389f8a5cc6ef5b
Richard Hanley <richard@rhanley.com>
2018-12-13 16:39:55 -0600

    Starting to redo encoding.  Feeling a lot better about it

--- src/edn_to_binary/core.clj
+++ src/edn_to_binary/core.clj
@@ -4,3 +4,64 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.set :as set]))
 
+(defprotocol Binary
+  (encode* [this encoding]))
+
+(s/def ::word-size #{1 2 4 8})
+(s/def ::order #{:little :big :network :native})
+(s/def ::flatten boolean?)
+
+(def order-map {:little ByteOrder/LITTLE_ENDIAN
+                :big ByteOrder/BIG_ENDIAN
+                :native (ByteOrder/nativeOrder)
+                :network ByteOrder/BIG_ENDIAN})
+
+(s/def ::base-encoding (s/keys :req [::word-size ::order ::flatten]))
+
+(def default-encoding {::word-size 1 
+                       ::order :little
+                       ::flaten true})
+
+(defn encode
+  ([data] (encode* data default-encoding))
+  ([data & encoding-key-vals] 
+   (let [encoding (apply array-map encoding-key-vals)]
+     (encode* data (merge default-encoding encoding)))))
+
+(defmacro put-prim [class put-method]
+  (let [size `(. ~class BYTES)]
+    `(fn [data# encoding#]
+       (let [{word-size# ::word-size
+              order# ::order} encoding#
+             buffer# (.order (ByteBuffer/allocate ~size)
+                            (get order-map order#))
+             align-to# (min word-size# ~size)]
+         (with-meta (seq (.array (~put-method buffer# data#)))
+                    {::alignment align-to#})))))
+
+(extend-protocol Binary
+  Byte
+  (encode* [this encoding] ((put-prim Byte .put) this encoding))
+  Short 
+  (encode* [this encoding] ((put-prim Short .putShort) this encoding))
+  Integer
+  (encode* [this encoding] ((put-prim Integer .putInt) this encoding))
+  Long
+  (encode* [this encoding] ((put-prim Long .putLong) this encoding))
+  Float
+  (encode* [this encoding] ((put-prim Float .putFloat) this encoding))
+  Double
+  (encode* [this encoding] ((put-prim Double .putDouble) this encoding)))
+
+(extend-protocol Binary
+  clojure.lang.Keyword
+  ;; If collections are being flattened, then a keyword should be encoded as nil
+  ;; Otherwise just return the keyword
+  (encode* [this encoding]
+    (if (not (::flatten encoding))
+      this))
+  clojure.lang.Sequential
+  clojure.lang.IPersistentMap)
+
+(defn alignment [binary-seq]
+  (or (::alignment (meta binary-seq)) 1))
--- test/edn_to_binary/core_test.clj
+++ test/edn_to_binary/core_test.clj
@@ -1,14 +1,70 @@
 (ns edn-to-binary.core-test
   (:require [clojure.test :refer :all]
-            [edn-to-binary.core :refer :all]))
+            [edn-to-binary.core :as bin]))
 
-(deftest simple-serialization
-  (let [expected-bytes (byte-array (map unchecked-byte (range 10)))]
-    (testing "single binary collection"
-      (is (= expected-bytes (serialize (range 10)))))
-    (testing "nested binary collections"
-      (is (= expected-bytes (serialize [[0] [1 2 3] [4 5] [6 7 8 9]]))))
-          ))
+(deftest primitive-encoding
+  (testing "little endian"
+    (testing "word-size 1"
+      (let [val (bin/encode (byte 12) ::bin/word-size 1 ::bin/order :little)]
+        (is (= [12] val))
+        (is (= 1 (bin/alignment val))))
+      (let [val (bin/encode (short 12) ::bin/word-size 1 ::bin/order :little)]
+        (is (= [12 0] val))
+        (is (= 1 (bin/alignment val))))
+      (let [val (bin/encode (int 0x01234567) ::bin/word-size 1 ::bin/order :little)]
+        (is (= [0x67 0x45 0x23 0x01] val))
+        (is (= 1 (bin/alignment val))))
+      (let [val (bin/encode (long 0x0123456776543210) ::bin/word-size 1 ::bin/order :little)]
+        (is (= [0x10 0x32 0x54 0x76 0x67 0x45 0x23 0x01] val))
+        (is (= 1 (bin/alignment val)))))
+    (testing "word-size 2"
+      (let [val (bin/encode (byte 12) ::bin/word-size 2 ::bin/order :little)]
+        (is (= 1 (bin/alignment val))))
+      (let [val (bin/encode (short 12) ::bin/word-size 2 ::bin/order :little)]
+        (is (= 2 (bin/alignment val))))
+      (let [val (bin/encode (int 0x01234567) ::bin/word-size 2 ::bin/order :little)]
+        (is (= 2 (bin/alignment val))))
+      (let [val (bin/encode (long 0x0123456776543210) ::bin/word-size 2 ::bin/order :little)]
+        (is (= 2 (bin/alignment val)))))
+    (testing "word-size 4"
+      (let [val (bin/encode (byte 12) ::bin/word-size 4 ::bin/order :little)]
+        (is (= 1 (bin/alignment val))))
+      (let [val (bin/encode (short 12) ::bin/word-size 4 ::bin/order :little)]
+        (is (= 2 (bin/alignment val))))
+      (let [val (bin/encode (int 0x01234567) ::bin/word-size 4 ::bin/order :little)]
+        (is (= 4 (bin/alignment val))))
+      (let [val (bin/encode (long 0x0123456776543210) ::bin/word-size 4 ::bin/order :little)]
+        (is (= 4 (bin/alignment val)))))
+    (testing "word-size 8"
+      (let [val (bin/encode (byte 12) ::bin/word-size 8 ::bin/order :little)]
+        (is (= 1 (bin/alignment val))))
+      (let [val (bin/encode (short 12) ::bin/word-size 8 ::bin/order :little)]
+        (is (= 2 (bin/alignment val))))
+      (let [val (bin/encode (int 0x01234567) ::bin/word-size 8 ::bin/order :little)]
+        (is (= 4 (bin/alignment val))))
+      (let [val (bin/encode (long 0x0123456776543210) ::bin/word-size 8 ::bin/order :little)]
+        (is (= 8 (bin/alignment val))))))
+  (testing "big endian"
+    (let [val (bin/encode (byte 12) ::bin/word-size 1 ::bin/order :big)]
+      (is (= [12] val))
+      (is (= 1 (bin/alignment val))))
+    (let [val (bin/encode (short 12) ::bin/word-size 1 ::bin/order :big)]
+      (is (= [0 12] val))
+      (is (= 1 (bin/alignment val))))
+    (let [val (bin/encode (int 0x01234567) ::bin/word-size 1 ::bin/order :big)]
+      (is (= [0x01 0x23 0x45 0x67]  val))
+      (is (= 1 (bin/alignment val))))
+    (let [val (bin/encode (long 0x0123456776543210) ::bin/word-size 1 ::bin/order :big)]
+      (is (= [0x01 0x23 0x45 0x67 0x76 0x54 0x32 0x10] val))
+      (is (= 1 (bin/alignment val))))))
+
+; (deftest simple-serialization
+;   (let [expected-bytes (byte-array (map unchecked-byte (range 10)))]
+;     (testing "single binary collection"
+;       (is (= expected-bytes (serialize (range 10)))))
+;     (testing "nested binary collections"
+;       (is (= expected-bytes (serialize [[0] [1 2 3] [4 5] [6 7 8 9]]))))
+;           ))
 
 
 ; (deftest a-test
